f86511a349f171344ee9e38de9c3cbe5105c36dc
Otávio Silva <otaviosilva2632@gmail.com>
2021-12-17 21:18:12 -0300

    feat: create structure of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailsFormComponent } from './payment-details/payment-details-form/payment-details-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailsFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/payment-details/payment-details-form/payment-details-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-details-form',
+  templateUrl: './payment-details-form.component.html',
+  styleUrls: ['./payment-details-form.component.sass']
+})
+export class PaymentDetailsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
